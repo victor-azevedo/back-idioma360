37cb96e4e96af23ae793bcd253352b1cbd7ef129
Victor Azevedo <josevcazevedo@gmail.com>
2023-03-23 16:33:48 -0300

    test: add invalid cpf generator test

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -1,7 +1,7 @@
 import { prisma } from "@/config";
 import { cities, states } from "@/helpers";
 
-export async function cleanDb() {
+export async function cleanUsersDb() {
   await prisma.userSession.deleteMany();
   await prisma.userAuth.deleteMany();
   await prisma.user.deleteMany();
@@ -21,16 +21,24 @@ export async function insertStatesDb() {
   });
 }
 
-export function cpfGenerator() {
-  const firstCpfField = randomCpfFieldGenerator();
-  const secondCpfField = randomCpfFieldGenerator();
-  const thirdCpfField = randomCpfFieldGenerator();
-  const checkDigit1 = checkDigitGenerator(firstCpfField, secondCpfField, thirdCpfField).toString();
-  const checkDigit2 = checkDigitGenerator(firstCpfField, secondCpfField, thirdCpfField, checkDigit1);
+export function cpfGeneratorValid() {
+  const firstCpfField = getRandomFieldNumericGenerator(3);
+  const secondCpfField = getRandomFieldNumericGenerator(3);
+  const thirdCpfField = getRandomFieldNumericGenerator(3);
+  const checkDigit1 = checkCpfDigitGenerator(firstCpfField, secondCpfField, thirdCpfField).toString();
+  const checkDigit2 = checkCpfDigitGenerator(firstCpfField, secondCpfField, thirdCpfField, checkDigit1);
   return `${firstCpfField}.${secondCpfField}.${thirdCpfField}-${checkDigit1}${checkDigit2}`;
 }
 
-function checkDigitGenerator(filed1: string, filed2: string, filed3: string, n4?: string) {
+export function cpfGeneratorInvalid() {
+  const validCpf = cpfGeneratorValid();
+  const firstDigit = parseInt(validCpf[0]);
+  const newFirstDigit = firstDigit === 9 ? firstDigit - 1 : firstDigit + 1;
+  const invalidCpf = newFirstDigit + validCpf.slice(1);
+  return invalidCpf;
+}
+
+function checkCpfDigitGenerator(filed1: string, filed2: string, filed3: string, n4?: string) {
   const digits = filed1.split("").concat(filed2.split(""), filed3.split(""));
   if (n4 !== undefined) {
     digits[9] = n4;
@@ -45,7 +53,21 @@ function checkDigitGenerator(filed1: string, filed2: string, filed3: string, n4?
   return y < 2 ? 0 : 11 - y;
 }
 
-function randomCpfFieldGenerator() {
-  const threeRandomDigits = Math.floor(Math.random() * 999);
-  return ("" + threeRandomDigits).padStart(3, "0");
+export function phoneGenerator() {
+  const dddField = getRandomFieldNumericGenerator(2);
+  const firstPhoneField = getRandomFieldNumericGenerator(getRandomIntInclusive(4, 5));
+  const secondPhoneField = getRandomFieldNumericGenerator(4);
+  return `(${dddField})${firstPhoneField}-${secondPhoneField}`;
+}
+
+function getRandomFieldNumericGenerator(digitsNumber: number) {
+  const factor = Math.pow(10, digitsNumber) - 1;
+  const fieldNumeric = Math.floor(Math.random() * factor);
+  return ("" + fieldNumeric).padStart(digitsNumber, "9");
+}
+
+function getRandomIntInclusive(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
