0e0d8f94f5107a9bc7475cba12dfba6f91a0756c
Victor Azevedo <josevcazevedo@gmail.com>
2023-03-27 18:56:53 -0300

    refactor: remove prisma-helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,5 +2,4 @@ export * from "./parseCitiesIBGE";
 export * from "./parses";
 export * from "./parseStatesIBGE";
 export * from "./patterns";
-export * from "./prisma-helper";
 export * from "./validations";
