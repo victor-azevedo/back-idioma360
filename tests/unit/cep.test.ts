11f0e8c4703a6af13d6ceb5d8c1e83829e435afb
Victor Azevedo <josevcazevedo@gmail.com>
2023-03-27 12:33:03 -0300

    test: add CEP validation

---
+++ tests/unit/cep.test.ts
@@ -0,0 +1,14 @@
+import { isValidCEP } from "@/helpers";
+
+describe("CEP validation", () => {
+  const validCEPs: string[] = ["76962-008", "59607-258", "56909-010", "64034-258"];
+  const invalidCEPs: string[] = ["00000-000", "12345-123", "11111-111", "99999-999"];
+
+  it("Check invalids CEPs", () => {
+    invalidCEPs.forEach(async (invalidCEP) => expect(await isValidCEP(invalidCEP)).toBe(false));
+  });
+
+  it("Check valid CEPs", async () => {
+    validCEPs.forEach(async (validCEP) => expect(await isValidCEP(validCEP)).toBe(true));
+  });
+});
