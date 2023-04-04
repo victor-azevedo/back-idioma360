429404723db516a8a535c697303c93810854ed3d
Victor Azevedo <josevcazevedo@gmail.com>
2023-04-04 13:42:50 -0300

    fix: enrollments query type

--- src/controllers/offerings-controller.ts
+++ src/controllers/offerings-controller.ts
@@ -1,4 +1,5 @@
 import { AuthenticatedRequest } from "@/middlewares";
+import { QuerySchema } from "@/schemas";
 import { offeringsService } from "@/services";
 import { Response } from "express";
 import httpStatus from "http-status";
@@ -6,10 +7,13 @@ import { handleRequestError } from "../middlewares";
 
 async function getAll(req: AuthenticatedRequest, res: Response) {
   const { userId } = req;
-  const { enrollments } = req.query;
+  const { enrollments } = req.query as QuerySchema;
 
   try {
-    const offerings = await offeringsService.findAll({ userId, includeEnrollments: enrollments ? true : false });
+    const offerings = await offeringsService.findAll({
+      userId,
+      includeEnrollments: enrollments === "true" ? true : false,
+    });
     return res.status(httpStatus.OK).send(offerings);
   } catch (error) {
     handleRequestError(error, res);
