b2bce0b75b379595232aec9eb5c35a33a159b48f
Victor Azevedo <josevcazevedo@gmail.com>
2023-04-13 13:10:05 -0300

    fix: time, status, offeringSchema

--- src/schemas/offerings-schema.ts
+++ src/schemas/offerings-schema.ts
@@ -5,7 +5,9 @@ import coreJoi, { ObjectSchema } from "joi";
 const Joi = coreJoi.extend(joiDate) as typeof coreJoi;
 
 export const offeringsQuerySchema: ObjectSchema = Joi.object<OfferingsQuerySchemaJoi>({
-  status: Joi.string().valid(Object.keys(OfferStatus).join(",")).optional(),
+  status: Joi.string()
+    .valid(...Object.keys(OfferStatus))
+    .optional(),
   enrollments: Joi.boolean().optional(),
 });
 
@@ -14,11 +16,13 @@ export const offeringSchema: ObjectSchema = Joi.object<OfferingBody>({
   startDate: Joi.date().format("YYYY-MM-DD").required(),
   endDate: Joi.date().format("YYYY-MM-DD").required(),
   testDate: Joi.date().format("YYYY-MM-DD").required(),
-  testStartTime: Joi.date().format("HH:MM").required(),
-  testEndTime: Joi.date().format("HH:MM").required(),
+  testStartTime: Joi.date().format("HH:mm").required(),
+  testEndTime: Joi.date().format("HH:mm").required(),
   resultDate: Joi.date().format("YYYY-MM-DD").required(),
   enrollPrice: Joi.number().min(0).required(),
-  status: Joi.string().valid(Object.keys(OfferStatus).join(",")).required(),
+  status: Joi.string()
+    .valid(...Object.keys(OfferStatus))
+    .required(),
 });
 
 export const offeringSchemaPatch: ObjectSchema = Joi.object<Partial<OfferingBody>>({
@@ -26,11 +30,13 @@ export const offeringSchemaPatch: ObjectSchema = Joi.object<Partial<OfferingBody
   startDate: Joi.date().format("YYYY-MM-DD").optional(),
   endDate: Joi.date().format("YYYY-MM-DD").optional(),
   testDate: Joi.date().format("YYYY-MM-DD").optional(),
-  testStartTime: Joi.date().format("HH:MM").optional(),
-  testEndTime: Joi.date().format("HH:MM").optional(),
+  testStartTime: Joi.date().format("HH:mm").optional(),
+  testEndTime: Joi.date().format("HH:mm").optional(),
   resultDate: Joi.date().format("YYYY-MM-DD").optional(),
   enrollPrice: Joi.number().min(0).optional(),
-  status: Joi.string().valid(Object.keys(OfferStatus).join(",")).optional(),
+  status: Joi.string()
+    .valid(...Object.keys(OfferStatus))
+    .optional(),
 });
 
 type OfferingsQuerySchemaJoi = {
